92c1c9ab07557151ead23b164cd0351ecd6df4ff
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-17 10:27:08 -0700

    building html

---
+++ assests /javascript.js
@@ -0,0 +1,8 @@
+// create a layout using the current day. This will use the moment 
+
+
+//create a function to Recognize when something is in the past, present or future.
+
+//create a button to add events on to the day 
+
+//
