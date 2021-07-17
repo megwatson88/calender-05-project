9267424cb61b3baa214f996ed660fbf4277c00de
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-17 11:05:56 -0700

    working on js

--- assests /javascript.js
+++ assests /javascript.js
@@ -1,6 +1,6 @@
 // create a layout using the current day. This will use the moment 
-
-
+let time
+moment().format('MMMM Dd YYYY')
 //create a function to Recognize when something is in the past, present or future.
 
 //create a button to add events on to the day 
