d0811bcdecd52d9ad8127c52d30d0592b88be035
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-18 16:31:27 -0700

    saving progress

--- assests /javascript.js
+++ assests /javascript.js
@@ -1,11 +1,14 @@
 
-var convertedDate = moment().format("MMM Do YY");
+var convertedDate = moment().format('MMM Do YYYY, h:mm:ss a ');
 document.getElementById("currentDay").innerHTML = convertedDate;
 
-let box = {};
+//let box = {};
 
-let createTask = function (taskText, taskTime) {
-    let taskLi = $("times").addClass("row");
-    let
+// let createTask = function (taskText, taskTime) {
+//     let taskLi = $("times").addClass("row");
+//     let
 
-}
+// };
+
+//let workHours = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6];
+
