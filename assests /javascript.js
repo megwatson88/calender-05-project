bcdc23bdd0623f32013b9b6ca0aeb3af6da3cf70
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-18 14:57:37 -0700

    JavaScript building

--- assests /javascript.js
+++ assests /javascript.js
@@ -1,8 +1,11 @@
-// create a layout using the current day. This will use the moment 
-let time
-moment().format('MMMM Dd YYYY')
-//create a function to Recognize when something is in the past, present or future.
 
-//create a button to add events on to the day 
+var convertedDate = moment().format("MMM Do YY");
+document.getElementById("currentDay").innerHTML = convertedDate;
 
-//
+let box = {};
+
+let createTask = function (taskText, taskTime) {
+    let taskLi = $("times").addClass("row");
+    let
+
+}
