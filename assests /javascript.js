54787058ae912f3ce8ac97f122b46ab25dd9694b
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-18 22:02:21 -0700

    jquesry issue fix

--- assests /javascript.js
+++ assests /javascript.js
@@ -1,17 +1,15 @@
 //display the time 
 let convertedDate = moment().format('MMM Do YYYY, h:mm:ss a ');
-document.getElementById("currentDay").innerHTML = convertedDate;
-
-let hourPlanner = document.getElementsByClassName('#task-add-button')
+document.getElementById('currentDay').innerHTML = convertedDate;
 //when the planning a task click on the button to add 
- hourPlanner.addEventListener('click', addPlans);
- function addPlans(){
- console.log("working")};
-    // event.preventDefalt()
-   // let hour = $(this).attr("id").split("-")[0]
-    // let planner = $('#'+ hour + "-plan").val()
-    // localStorage.setItem(hour, planner)
-    // console.log("working")
- //});
-//loop for knowing the time of day 
- //for (let i=8; i=18;i++){}
+$('.task-add-button').on('click', function () {
+   function addPlans() {
+      console.log("1")
+
+      let hour = $(this).attr("id").split("-")[0]
+      let planner = $('#' + hour + "-plan").val()
+      localStorage.setItem(hour, planner)
+      console.log("working")
+   }});
+   //loop for knowing the time of day 
+  // for (let i = 8; i = 18; i++) 
