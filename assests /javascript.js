e33103b390cac49f3b89b206c5fec3a0d41b14a0
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-20 13:14:47 -0700

    html changes and js updates

--- assests /javascript.js
+++ assests /javascript.js
@@ -2,14 +2,10 @@
 let convertedDate = moment().format('MMM Do YYYY, h:mm:ss a ');
 document.getElementById('currentDay').innerHTML = convertedDate;
 //when the planning a task click on the button to add 
-$('.task-add-button').on('click', function () {
-   function addPlans() {
-      console.log("1")
-
-      let hour = $(this).attr("id").split("-")[0]
-      let planner = $('#' + hour + "-plan").val()
-      localStorage.setItem(hour, planner)
-      console.log("working")
-   }});
-   //loop for knowing the time of day 
-  // for (let i = 8; i = 18; i++) 
+$('.save-button').on('click', function(event) {
+    event.preventDefaut()
+   console.log("1")
+});
+//$('save-button').on('click', function(save));
+//loop for knowing the time of day 
+//for (let i = 8; i = 18; i++) {}
