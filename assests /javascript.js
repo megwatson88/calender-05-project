2e12b0cdcf9a103086c19c0a4439adc59eac355a
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-20 18:37:46 -0700

    additions

--- assests /javascript.js
+++ assests /javascript.js
@@ -1,6 +1,14 @@
 //display the time 
 let convertedDate = moment().format('MMM Do YYYY, h:mm:ss a ');
 document.getElementById('currentDay').innerHTML = convertedDate;
+var list = JSON.parse(localStorage.getItem('todolist')) || [];
+function renderTodo(list){
+   $('#save-button').empty();
+   for (var i= 0; i<list.length; i++){
+      var toDoItem = $('form-control');
+      toDoItem.text(list[i]);
+   }
+};
 //when the planning a task click on the button to add 
 $('.save-button').on('click', function(event) {
     event.preventDefaut()
