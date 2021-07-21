2f8dde137eb11fe976495a06f0dca28e345e5aa1
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-21 12:49:52 -0700

    deletions of JS

--- assests /javascript.js
+++ assests /javascript.js
@@ -1,6 +1,8 @@
 //display the time 
 let convertedDate = moment().format('MMM Do YYYY, h:mm:ss a ');
 document.getElementById('currentDay').innerHTML = convertedDate;
+
+//list out the tasks 
 var list = JSON.parse(localStorage.getItem('todolist')) || [];
 function renderTodo(list){
    $('#save-button').empty();
@@ -9,11 +11,11 @@ function renderTodo(list){
       toDoItem.text(list[i]);
    }
 };
+
 //when the planning a task click on the button to add 
 $('.save-button').on('click', function(event) {
     event.preventDefaut()
    console.log("1")
+   localStorage.setItem("todoList")
+   
 });
-//$('save-button').on('click', function(save));
-//loop for knowing the time of day 
-//for (let i = 8; i = 18; i++) {}
